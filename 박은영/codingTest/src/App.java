7225e7c6ea8464bc847ffbec301aee5c4e4feed2
Ey <dmss3@naver.com>
2023-04-18 23:03:23 +0900

    commit_test

---
+++ 박은영/codingTest/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String test = sc.nextLine();
+        System.out.print(test);
+        //System.out.println("Hello, World!");
+    }
+}
